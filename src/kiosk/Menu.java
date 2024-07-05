4d297d1a8d1e0d3128cf82715169c1713a36df6d
sei <wdw7448@gmail.com>
2024-07-05 18:21:04 +0900

    first commit

---
+++ src/kiosk/Menu.java
@@ -0,0 +1,16 @@
+package kiosk;
+
+public class Menu {
+    // 이름, 설명 필드를 가지는 클래스
+    protected String name;
+    protected String explanation;
+
+    public Menu(String name, String explanation) {
+        this.name = name;
+        this.explanation = explanation;
+    }
+
+    public String show() {
+        return String.format("%-10s | %s", name, explanation);
+    }
+}
